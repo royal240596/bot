f10cc384a3f69f9c393ce2817f932a59fd83040a
Royal Bhati <royal240596@gmail.com>
2018-01-27 16:33:30 +0530

    Catch exception and faster

--- reddit_bot.py
+++ reddit_bot.py
@@ -4,29 +4,28 @@ import config
 import time
 
 def bot_login():
-	print "Loggin in..."
+	
 	r = praw.Reddit(username = config.username,
 			password = config.password,
 			client_id = config.client_id,
 			client_secret = config.client_secret,
 			user_agent = "boty-boop's odgovara v0.1")
-	print "Logged in!"
 
 	return r
 
 def run_bot(r):
-	print "Obtaining 25 comments..."
-	for comment in r.subreddit('Test_Posts').comments(limit=25):
-		if "dog" in comment.body:
-			print "String with \"dog\" found in comment " + comment.id
-			comment.reply("I also love dogs! [Here](http://i.imgur.com/LLgRKeq.jpg) is an image of one!")
-			print "Replied to comment "
-
+	print("Obtaining 25 comments...")
+	
 	print "Sleeping for 10 seconds..."
-	#Sleep for 10 seconds...
+	try:
+		[comment.reply("I also love dogs! [Here](http://i.imgur.com/LLgRKeq.jpg) is an image of one!") for comment in r.subreddit('Test_Posts').comments(limit=25) if "dog" in comment.body]
+
+	except:
+		exception('Cannot comment!')
+			
 	time.sleep(10)
 
-print "reddit"
+
 r = bot_login()
 while True:
 	run_bot(r)
